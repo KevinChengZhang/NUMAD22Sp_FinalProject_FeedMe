af532f4cd95c5195267234494cc7212f60a18edc
Kevin Zhang <kevinchenzhang@gmail.com>
2022-04-19 19:47:03 -0400

    init commit

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_finalproject_feedme/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad22sp_finalproject_feedme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
