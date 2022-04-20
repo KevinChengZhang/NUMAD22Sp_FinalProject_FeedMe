03d0e202ba9622ca0aaff72c431e5afd89a6acdb
Kevin Zhang <kevinchenzhang@gmail.com>
2022-04-19 22:40:13 -0400

    adding basic yelp api implementation

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_finalproject_feedme/Yelp/YelpApi.java
@@ -0,0 +1,86 @@
+package edu.neu.madcourse.numad22sp_finalproject_feedme.Yelp;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YelpApi {
+    private static final String TAG = "YelpAPI";
+
+    private String base = "https://api.yelp.com/v3/businesses/search?";
+    private String apiKey = "HBD2H-YxkqKy917O4ps7Ndp5XjirUXe3MpuCjo6bmx5RLXCgGoc-NtmrRh6IUkJU95R8i-aL4Rqfbsk9GdHBoR4DwvTGBbnR72tVrdLeZtUpWGt4MuezSq0VOzUQYXYx";
+    private HttpURLConnection conn;
+    private List<YelpBusiness> businesses;
+
+    public YelpApi() {
+        businesses = new ArrayList<YelpBusiness>();
+    }
+
+    /**
+     * Get the first 50 businesses in Yelp Search.
+     * Yelp Business Search Docs: https://www.yelp.com/developers/documentation/v3/business_search
+     * @param term the search term
+     * @param location the geographic area to be used when searching for businesses
+     * @param price the pricing levels to filter the search result with: 1 = $, 2 = $$, 3 = $$$, 4 = $$$$.
+     *              The price filter can be a list of comma delimited pricing levels.
+     * @return An array list of Yelp Businesses from Yelp API search result
+     */
+    public List<YelpBusiness> getBusinesses(String term, String location, String price) {
+        BufferedReader reader;
+        String line;
+        StringBuilder responseContent = new StringBuilder();
+
+        try{
+            Log.e(TAG, "Running HTTP GET Request...");
+            URL url = new URL(String.format(base + "term=%s&location=%s&price=%s", term, location, price));
+            Log.e(TAG, String.valueOf(url));
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.addRequestProperty("Authorization", "Bearer " + apiKey);
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(5000);
+            Log.e(TAG, "status code for yelp api: "+ conn.getResponseCode());
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                responseContent.append(line);
+                System.out.println("line " + line);
+            }
+            reader.close();
+
+            JSONObject jObject =  new JSONObject(responseContent.toString());
+            JSONArray jsonArray = jObject.getJSONArray("businesses");
+
+            Log.e(TAG, String.valueOf(jsonArray));
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                YelpBusiness business = new YelpBusiness(jsonArray.getJSONObject(i));
+                businesses.add(business);
+            }
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return businesses;
+    }
+
+}
---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_finalproject_feedme/Yelp/YelpBusiness.java
@@ -0,0 +1,68 @@
+package edu.neu.madcourse.numad22sp_finalproject_feedme.Yelp;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class YelpBusiness {
+    private double rating;
+    private String price;
+    private String phone;
+    private String id;
+    private String alias;
+    private boolean isClosed;
+    private HashMap<String, String> categories;
+    private int reviewCount;
+    private String name;
+    private String url;
+    private HashMap<String, String> coordinates;
+    private String imageUrl;
+    private HashMap<String, String> location;
+    private double distance;
+    private List<String> transactions;
+
+    public YelpBusiness(JSONObject business) throws JSONException {
+        try {
+            rating = business.getInt("rating");
+            price = business.getString("price");
+            phone = business.getString("phone");
+            id = business.getString("id");
+            alias = business.getString("alias");
+            isClosed = business.getBoolean("is_closed");
+            reviewCount = business.getInt("review_count");
+            name = business.getString("name");
+            url = business.getString("url");
+            imageUrl = business.getString("image_url");
+            distance = business.getDouble("distance");
+
+            categories = new HashMap<String, String>();
+            categories.put("alias", business.getJSONObject("categories").getString("alias"));
+            categories.put("title", business.getJSONObject("categories").getString("title"));
+
+            coordinates = new HashMap<String, String>();
+            categories.put("latitude", business.getJSONObject("coordinates").getString("latitude"));
+            categories.put("latitude", business.getJSONObject("coordinates").getString("latitude"));
+
+            location = new HashMap<String, String>();
+            location.put("city", business.getJSONObject("location").getString("city"));
+            location.put("country", business.getJSONObject("location").getString("country"));
+            location.put("address2", business.getJSONObject("location").getString("address2"));
+            location.put("address3", business.getJSONObject("location").getString("address3"));
+            location.put("state", business.getJSONObject("location").getString("state"));
+            location.put("address1", business.getJSONObject("location").getString("address1"));
+            location.put("zip_code", business.getJSONObject("location").getString("zip_code"));
+
+            // Probably an easier and more efficient way by using Jackson
+            JSONArray jsonTransactions = business.getJSONArray("transactions");
+            for (int i = 0; i < jsonTransactions.length(); i++) {
+                transactions.add(jsonTransactions.get(i).toString());
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
