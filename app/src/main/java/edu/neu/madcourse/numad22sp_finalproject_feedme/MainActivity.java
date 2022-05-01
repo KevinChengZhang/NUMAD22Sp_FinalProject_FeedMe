9322f7142ab04a612148df471a91c2343960a3c0
Kevin Zhang <kevinchenzhang@gmail.com>
2022-05-01 16:06:58 -0400

    add ui to main activity

--- app/src/main/java/edu/neu/madcourse/numad22sp_finalproject_feedme/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_finalproject_feedme/MainActivity.java
@@ -2,26 +2,43 @@ package edu.neu.madcourse.numad22sp_finalproject_feedme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 import edu.neu.madcourse.numad22sp_finalproject_feedme.Friends.FindFriendsActivity;
+import edu.neu.madcourse.numad22sp_finalproject_feedme.MainFeed.FeedAdapter;
 import edu.neu.madcourse.numad22sp_finalproject_feedme.MainFeed.MainFeed;
 import edu.neu.madcourse.numad22sp_finalproject_feedme.MakeRecommendation.MakeRecommendationActivity;
 import edu.neu.madcourse.numad22sp_finalproject_feedme.UserProfile.UserProfile;
@@ -35,8 +52,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String userEmail = "";
 
     private HashMap<Integer, String> priceRange = new HashMap<Integer, String>();
+    List<Boolean> states = Arrays.asList(false, false, false, false);
     private String wanted_price;
     private Handler resultHandler = new Handler();
+    private AlertDialog alertDialog;
 
     private EditText cuisine;
     private EditText location;
@@ -66,10 +85,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initControl() {
-        CheckBox price1 = findViewById(R.id.yelp_price_1);
-        CheckBox price2 = findViewById(R.id.yelp_price_2);
-        CheckBox price3 = findViewById(R.id.yelp_price_3);
-        CheckBox price4 = findViewById(R.id.yelp_price_4);
+        Button price1 = findViewById(R.id.yelp_price_1);
+        Button price2 = findViewById(R.id.yelp_price_2);
+        Button price3 = findViewById(R.id.yelp_price_3);
+        Button price4 = findViewById(R.id.yelp_price_4);
+        List<Button> buttons = Arrays.asList(price1, price2, price3, price4);
+        for(int i = 0; i < buttons.size(); i++) {
+            if(!states.get(i)) {
+                buttons.get(i).setBackgroundColor(Color.GRAY);
+            }
+        }
+
         Button search = findViewById(R.id.yelp_search);
         Button profileButton = findViewById(R.id.profileButton);
         Button friendsButton = findViewById(R.id.friendsButton);
@@ -96,19 +122,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.yelp_price_1:
-                onCheckboxClicked(findViewById(R.id.yelp_price_1), 1);
+                onPriceClicked(findViewById(R.id.yelp_price_1), 1);
                 break;
 
             case R.id.yelp_price_2:
-                onCheckboxClicked(findViewById(R.id.yelp_price_2), 2);
+                onPriceClicked(findViewById(R.id.yelp_price_2), 2);
                 break;
 
             case R.id.yelp_price_3:
-                onCheckboxClicked(findViewById(R.id.yelp_price_3), 3);
+                onPriceClicked(findViewById(R.id.yelp_price_3), 3);
                 break;
 
             case R.id.yelp_price_4:
-                onCheckboxClicked(findViewById(R.id.yelp_price_4), 4);
+                onPriceClicked(findViewById(R.id.yelp_price_4), 4);
                 break;
 
             case R.id.yelp_search:
@@ -160,8 +186,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         @Override
         public void run() {
+            String food = (cuisine.getText().toString().length() == 0) ? "food" : cuisine.getText().toString();
+            String loc = (location.getText().toString().length() == 0) ? "Boston" : location.getText().toString();
             yelpApiClient = new YelpApiClient();
-            List<YelpBusiness> bus = yelpApiClient.getBusinesses(cuisine.getText().toString(), location.getText().toString(), wanted_price);
+            List<YelpBusiness> bus = yelpApiClient.getBusinesses(food, loc, wanted_price);
 
             // In this basic yelp api implementation, yelp will return the same JSON every time
             // if the parameters aren't changed.
@@ -182,15 +210,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     // Randomly pick a business from a returned JSON
                     String bus_name = bus.get(rng).getName();
                     result.setText("Result: " + bus_name);
+                    buildFilterPopup(bus.get(rng));
                 });
             }
         }
     }
 
-    private void onCheckboxClicked(View view, int price) {
-        boolean checked = ((CheckBox) view).isChecked();
-        String new_price = (checked) ? Integer.toString(price) : "";
-        priceRange.put(price, new_price);
+    private void onPriceClicked(View view, int price) {
+        boolean wasActive = states.get(price-1);
+
+        if(wasActive) {
+            view.setBackgroundColor(Color.GRAY);
+            priceRange.put(price, "");
+        } else {
+            TypedValue typedValue = new TypedValue();
+            getTheme().resolveAttribute(androidx.appcompat.R.attr.colorPrimary, typedValue, true);
+            int color = typedValue.data;
+            view.setBackgroundColor(color);
+            priceRange.put(price, Integer.toString(price));
+        }
+        states.set(price-1, !wasActive);
     }
 
+    private void buildFilterPopup(YelpBusiness business) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final View contactPopupView = getLayoutInflater().inflate(R.layout.main_feed_card, null);
+
+        TextView view_title, view_description, view_number, view_address;
+        RatingBar view_rating;
+        ImageView view_image;
+
+        view_title = contactPopupView.findViewById(R.id.cardTitle);
+        view_description = contactPopupView.findViewById(R.id.cardDescription);
+        view_number = contactPopupView.findViewById(R.id.cardNumber);
+        view_rating = contactPopupView.findViewById(R.id.cardRating);
+        view_image = contactPopupView.findViewById(R.id.cardIV);
+        view_address = contactPopupView.findViewById(R.id.cardAddress);
+
+        new DownloadImageFromInternet(view_image).execute(business.getImageUrl());
+
+        String number = business.getPhone();
+        if(number.length() >= 12) {
+            view_number.setText(String.format("(%s) %s-%s", number.substring(2, 5), number.substring(5, 8),
+                    number.substring(8, 12)));
+        }
+        view_title.setText(business.getName());
+        view_rating.setRating((float) business.getRating());
+
+        String description = business.getPrice()
+                + " ★ "
+                + business.getCategories().get("title")
+                + " ★ "
+                + String.join("/", business.getTransactions());
+        view_description.setText(description);
+
+        String address = business.getLocation().get("address1")
+                + ", "
+                + business.getLocation().get("city")
+                + " "
+                + business.getLocation().get("zip_code");
+        view_address.setText(address);
+        builder.setView(contactPopupView);
+
+        alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView=imageView;
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL=urls[0];
+            Bitmap bimage=null;
+            try {
+                InputStream in=new java.net.URL(imageURL).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
 }
